cb4a150d2e926e4f272657a96c6cb11d7c80dc13
KO_Paing_Lay <89724508+sittpaing123@users.noreply.github.com>
2022-09-26 15:20:46 +0630

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 import re
 from os import environ
 
-#start
+
 PICS = (environ.get('PICS', 'https://telegra.ph/file/14c978d31fb9372e79624.jpg https://telegra.ph/file/a5ee7fc4b2d89fe900321.jpg')).split()
 PICS2 = (environ.get('PICS', 'https://telegra.ph/file/14c978d31fb9372e79624.jpg https://telegra.ph/file/a5ee7fc4b2d89fe900321.jpg')).split()
