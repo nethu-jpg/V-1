a195c5cf9e769674170f989390d41f157a0703e1
KO_Paing_Lay <107101116+Kopaing12@users.noreply.github.com>
2022-12-21 13:22:22 +0630

    Update filters.py
    Signed-off-by: KO_Paing_Lay <107101116+Kopaing12@users.noreply.github.com>

--- plugins/filters.py
+++ plugins/filters.py
@@ -350,7 +350,7 @@ async def delallconfirm(client, message):
 
 
  
-@Client.on_message(filters.group & filters.text & ~filters.edited & filters.incoming)
+@Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
     k = await manual_filters(client, message)
     if k == False:
