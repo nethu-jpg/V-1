40257560529c1601d30cd6b41d0ef888474a1f6a
KO_Paing_Lay <107101116+Kopaing12@users.noreply.github.com>
2022-12-21 13:17:18 +0630

    Update config.py
    Signed-off-by: KO_Paing_Lay <107101116+Kopaing12@users.noreply.github.com>

--- config.py
+++ config.py
@@ -6,40 +6,40 @@ from logging.handlers import RotatingFileHandler
 
 
 # Get a bot token from botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5806016246:AAE_C1NtmjSpgFRk8pd1v0nHbOkFbVxHn8k")
 
 # Get from my.telegram.org (or @UseTGXBot)
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "7880210"))
 
 # Get from my.telegram.org (or @UseTGXBot)
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "1bb4b2ff1489cc06af37cba448c8cce9")
 
 # Generate a user session string 
-TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
+TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "BQC7m_aSlwlhua-XDK0rildU0WFwtt31aq0Z8Ruyn3lh3UbzluUzdfdpxDz-IN3juiGGrovcmK-MwiYbobLoSfdUQcilgWSaMaqgNqpii_nUncs6Vu7vIOHp8JcsQ5E0CneipTD3FdhAHGf_lVC9pkVvavH4ijFMZl0pmQnDyKYU-Ogeu5OOo7FOj6j_SUfG2_A3SOk-rJxm_lcTskQ7btlLgqbkMQR3FtpR02leBeBR-52m8AnAFp4mlnPei-CzcmRGg9m1LFcUe_CGcpfD3AhoZOFIXQzogJLTl6cCcjslZCqQ5b9aDGc6d0iZW9Imj2YWMtKte8YG589xI68Mf4VuYbzVEgA")
 
 # Database URL from https://cloud.mongodb.com/
-DATABASE_URI = os.environ.get("DATABASE_URI", "")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://bot1:bot1@cluster0.fqzak9o.mongodb.net/?retryWrites=true&w=majority")
 
 # Your database name from mongoDB
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 DATABASE_NAME2 = str(os.environ.get("DATABASE_NAME2", "Unlimited"))
 
 # ID of users that can use the bot commands
-AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1113630298").split())
 
 # Should bot search for document files in channels
 DOC_SEARCH = os.environ.get("DOC_SEARCH", "yes").lower()
 
 # Should bot search for video files in channels
-VID_SEARCH = os.environ.get("VID_SEARCH", "no").lower()
+VID_SEARCH = os.environ.get("VID_SEARCH", "yes").lower()
 
 # Should bot search for music files in channels
-MUSIC_SEARCH = os.environ.get("MUSIC_SEARCH", "no").lower()
+MUSIC_SEARCH = os.environ.get("MUSIC_SEARCH", "yes").lower()
 
 # To save user details (Usefull for getting userinfo and total user counts)
 # May reduce filter capacity :(
 # Give yes or no
-SAVE_USER = os.environ.get("SAVE_USER", "no").lower()
+SAVE_USER = os.environ.get("SAVE_USER", "yes").lower()
 
 
 # Go to https://dashboard.heroku.com/account, scroll down and press Reveal API
